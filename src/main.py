188a440a8f3e84b61c45736596f6bc44bd287e7e
Basil <techiebasil@outlook.com>
2025-09-09 00:28:21 +0530

    fix package import error

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from utils.logger import logger
 from utils.docker_logs_extract import stream_container_logs
 import os
-from src.task_executor import run_agent
+from task_executor import run_agent
 
 def main():
     for log in stream_container_logs("book-service"):
